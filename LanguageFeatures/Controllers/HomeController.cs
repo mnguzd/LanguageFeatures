ddc0c2e8b93597ed1e587513835a95dd3efa28df
mnguzd <roman.kioroglo@gmail.com>
2020-08-04 18:07:26 +0300

    Properties and readonly

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -4,16 +4,17 @@ using System.Collections.Generic;
 
 namespace LanguageFeatures.Controllers
 {
-    public class HomeController:Controller
+    public class HomeController : Controller
     {
         public ViewResult Index()
         {
             List<string> results = new List<string>();
-            foreach(Product p in Product.GetProducts())
+            foreach (Product p in Product.GetProducts())
             {
-                string name = p?.Name;
-                decimal? price = p?.Price;
-                results.Add(string.Format("Name:{0},Price:{1}", name, price));
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                results.Add(string.Format("Name:{0}, Price:{1}, Related:{2}", name, price, relatedName));
             }
             return View(results);
         }
